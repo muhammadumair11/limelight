01d1096ccc03698f9fefc6564cf15e8888f8f61b
Muhammad Umair <47901848+muhammadumair11@users.noreply.github.com>
2022-03-03 12:30:41 +0500

    Revert "signupform"

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>LimeLight</title>
+  <title>Bootstrap Site</title>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script>
@@ -13,6 +13,19 @@
 <body>
 
   <?php include('Includes/Header/navbar.php') ?>
+  <br>
+  <form class="container">
+  <div class="form-group col-lg-6">
+    <input type="email" class="form-control" aria-describedby="emailHelp" placeholder="Enter email">
+  </div>
+  <br>
+  <div class="form-group col-lg-6">
+  <input type="password" class="form-control" placeholder="Password">
+  </div>
+  <div class="form-group form-check">
+  </div>
+<button type="submit" class="btn btn-primary">Submit</button>
+</form>
     
 </body>
 </html>
